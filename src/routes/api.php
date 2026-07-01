7f8e4374e40323953737edbd4c9a518322db5b99
Amirhossein Jahani <me@iamir.net>
2026-07-01 08:41:45 +0330

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -9,9 +9,9 @@
 
 Route::namespace('v1')->prefix('v1')->middleware('authIf:api')->group(function () {
     Route::apiResource('fundraising_campaigns', 'FundraisingCampaignController', ['as' => 'api']);
-    Route::get('fundraising_campaigns/{record}/subscribe', 'FundraisingCampaignController@subscribe')->name('api.fundraising_campaigns.subscribe');
-    Route::get('fundraising_campaigns/{record}/unsubscribe', 'FundraisingCampaignController@unsubscribe')->name('api.fundraising_campaigns.unsubscribe');
-    Route::get('fundraising_campaigns/{record}/pay', 'FundraisingCampaignController@pay')->name('api.fundraising_campaigns.pay');
+    Route::post('fundraising_campaigns/{record}/subscribe', 'FundraisingCampaignController@subscribe')->name('api.fundraising_campaigns.subscribe');
+    Route::post('fundraising_campaigns/{record}/unsubscribe', 'FundraisingCampaignController@unsubscribe')->name('api.fundraising_campaigns.unsubscribe');
+    Route::post('fundraising_campaigns/{record}/pay', 'FundraisingCampaignController@pay')->name('api.fundraising_campaigns.pay');
     Route::apiResource('fundraising_subscribers', 'FundraisingSubscriberController', ['as' => 'api']);
     Route::apiResource('fundraising_donations', 'FundraisingDonationController', ['as' => 'api']);
 });
